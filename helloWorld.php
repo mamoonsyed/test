badc7f3f5872f56df6741cc9b2053906143f77ed
transmatter <transmatter99@gmail.com>
2016-03-31 18:02:52 +0500

    Added files via upload

---
+++ helloWorld.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>
+			PHP static application.
+		</title>
+	</head>
+	<body>
+		<P>
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+		</p>
+	</body>
+</html>
